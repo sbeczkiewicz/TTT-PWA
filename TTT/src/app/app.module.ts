8457997cf087c9fb75cb63c11ff458ed514612c4
sbeczkiewicz <sbeczkiewicz@wisc.edu>
2021-04-05 00:05:26 -0500

    Deploy and make into PWA

--- TTT/src/app/app.module.ts
+++ TTT/src/app/app.module.ts
@@ -25,7 +25,13 @@ import { environment } from '../environments/environment';
     NbLayoutModule,
     NbEvaIconsModule,
     NbButtonModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
